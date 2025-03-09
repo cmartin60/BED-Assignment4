868785d7c3854bf5b54fda79380fd05f3fc74bde
Christian Martin <cmartin60@academic.rrc.ca>
2025-03-09 15:06:04 -0500

    Update: Controller, Routes, app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,9 @@ import morgan from "morgan";
 // import setupSwagger endpoint
 import setupSwagger from "../config/swagger";
 import itemRoutes from "./api/v1/routes/itemRoutes";
-
+import userRoutes from "./api/v1/routes/userRoutes";
+import adminRoutes from "./api/v1/routes/adminRoutes";
+import errorHandler from "./api/v1/middleware/errorHandler";
 
 // initialize the express application
 const app: Express = express();
@@ -58,7 +60,10 @@ app.get("/api/v1/health", (req, res) => {
 });
 
 app.use("/api/v1/items", itemRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/admin", adminRoutes);
 
+app.use(errorHandler);
 
 // export app and server for testing
 export default app;
